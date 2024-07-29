1ae8901868ad0866b98a441dddb99db7487cf982
Nadun <it21216046@my.sliit.lk>
2024-07-29 23:50:55 +0530

    chat api

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react'
 
+//const API_KEY = "sk-proj-RfSMvM0lFhGfN3iqqRQOT3BlbkFJJtdUQTzcclvIdSeZ1ohg";
+
 function App() {
   const [typing, setTyping] = useState(false)
   const [messages, setMessages] = useState([
@@ -27,6 +29,60 @@ function App() {
     setMessages(newMessages);
 
     setTyping(true);
+
+    await processMessagetoBanshee(newMessages) // process message to the gpt (send it and response)
+
+  }
+
+  async function processMessagetoBanshee(chatMessage) {
+    //charMeassage { sender: "user" or "Banshee", message: "message content"}
+    //apiMessage { role: "user" or "assistant", content: "message content"}
+
+    let apiMessages = chatMessage.map((messageObject) =>{
+      let role = "";
+      if(messageObject.sender === "Banshee"){
+        role = "assistant"
+      }else{
+        role = "user"
+      }
+      return { role: role, content: messageObject.message }
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "Speak like a pirate"
+    }
+
+    const apiBody = {
+      "model": "gpt-3.5-turbo",
+      "messages":[
+        ...apiMessages 
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiBody)
+    }
+      
+
+    ).then((data) =>{
+      return data.json();
+    }).then((data) =>{
+      console.log(data)
+      console.log(data.choices[0].messsage.content);
+      setMessages(
+        [...chatMessage, {
+          message: data.choices[0].message.content,
+          sender: "Banshee"
+        }]
+      );
+      setTyping(false);
+    });
 
   }
 
